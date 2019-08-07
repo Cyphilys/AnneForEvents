36bb09d1bce3ffad3d38a9d55ab43eb1cf3e1305
Mickael kerkhove <mickael.kerkhove@coding-academy.fr>
2019-08-07 17:36:37 +0200

    fix footer link

--- src/components/layout.js
+++ src/components/layout.js
@@ -115,9 +115,9 @@ const { Text, Title } = Typography;
           </div>
           <br/>
           <div>
-            <a href="https://www.facebook.com/" ><Icon type="facebook" theme="filled" style={{ fontSize: '35px', color: '#009ACD', marginRight: '0.5rem'}}/></a>
-            <a href="https://www.instagram.com/?hl=fr" ><Icon type="instagram" style={{ fontSize: '35px', color: '#009ACD', marginRight: '0.5rem'}}/></a>
-            <a href="https://twitter.com/?lang=fr" ><Icon type="twitter" style={{ fontSize: '35px', color: '#009ACD'}}/></a>
+            <a href="https://www.facebook.com/" target="_blank" rel="noopener noreferrer"><Icon type="facebook" theme="filled" style={{ fontSize: '35px', color: '#009ACD', marginRight: '0.5rem'}}/></a>
+            <a href="https://www.instagram.com/?hl=fr" target="_blank" rel="noopener noreferrer"><Icon type="instagram" style={{ fontSize: '35px', color: '#009ACD', marginRight: '0.5rem'}}/></a>
+            <a href="https://twitter.com/?lang=fr" target="_blank" rel="noopener noreferrer"><Icon type="twitter" style={{ fontSize: '35px', color: '#009ACD'}}/></a>
           </div>
         </div>
       </Footer>
