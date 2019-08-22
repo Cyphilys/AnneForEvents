5be7f3f0bde7071355875cd597133c7dba46cd16
Mickael kerkhove <mickael.kerkhove@coding-academy.fr>
2019-08-22 09:24:31 +0200

    Flex menu

--- src/components/layout.js
+++ src/components/layout.js
@@ -9,7 +9,7 @@ import React from "react"
 import PropTypes from "prop-types"
 import { useStaticQuery, graphql, Link } from "gatsby"
 
-import { Layout, Menu, Icon, Typography } from "antd"
+import { Layout, Menu, Icon, Typography, Row, Col } from "antd"
 
 import "../css/global.css"
 
@@ -36,52 +36,54 @@ const { Text } = Typography;
   return (
     <Layout style={{ minHeight: '100vh', background: "rgb(255, 255, 255)" }}>
       <Header style={{ position: 'fixed', zIndex: 1, width: '100%', background: '#FFF' }}>
-        <div className="ant-col-6">
-          <a>
-            <Link to="/">
-              <Logo/></Link>
-          </a>
-        </div>
-        <div className="ant-col-18 highscreen">
-          <Menu
-            theme="light"
-            mode="horizontal"
-            defaultSelectedKeys={[page]}
-            style={{ lineHeight: '64px', fontSize: '16px', textAlign: 'right' }}
-          >
-            <Menu.Item key="1" >
-              <Link to="/" />
-              Accueil</Menu.Item>
-            <Menu.Item key="2">
-              <Link to="/page-2" />
-              Prestations artistiques</Menu.Item>
-            <Menu.Item key="3">
-              <Link to="/" />
-              Location animaux</Menu.Item>
-            <SubMenu
-              title={
-                <span className="submenu-title-wrapper">
-                Notre expertise - Vos projets
-              </span>
-              }
+        <Row type="flex" justify="space-between">
+          <Col>
+            <a>
+              <Link to="/">
+                <Logo/></Link>
+            </a>
+          </Col>
+          <Col className="highscreen">
+            <Menu
+              theme="light"
+              mode="horizontal"
+              defaultSelectedKeys={[page]}
+              style={{ lineHeight: '64px', fontSize: '16px', textAlign: 'right' }}
             >
-              <Menu.ItemGroup>
-                <Menu.Item key="4" style={{ fontSize: '16px' }}>Vous êtes un particulier</Menu.Item>
-                <Menu.Item key="5" style={{ fontSize: '16px' }}>Vous êtes une collectivité</Menu.Item>
-                <Menu.Item key="6" style={{ fontSize: '16px' }}>Vous êtes organisateur de foires et salons</Menu.Item>
-                <Menu.Item key="7" style={{ fontSize: '16px' }}>Vous êtes un comité d'entreprise</Menu.Item>
-                <Menu.Item key="8" style={{ fontSize: '16px' }}>Vous êtes une entreprise</Menu.Item>
-                <Menu.Item key="9" style={{ fontSize: '16px' }}>Vous êtes un centre commercial</Menu.Item>
-              </Menu.ItemGroup>
-            </SubMenu>
-            <Menu.Item key="10">
-              <Link to="/about" />
-              Qui sommes nous ?</Menu.Item>
-          </Menu>
-        </div>
-        <div className="ant-col-18 mobile">
-          <LittleMenu/>
-        </div>
+              <Menu.Item key="1" >
+                <Link to="/" />
+                Accueil</Menu.Item>
+              <Menu.Item key="2">
+                <Link to="/page-2" />
+                Prestations artistiques</Menu.Item>
+              <Menu.Item key="3">
+                <Link to="/" />
+                Location animaux</Menu.Item>
+              <SubMenu
+                title={
+                  <span className="submenu-title-wrapper">
+                  Notre expertise - Vos projets
+                </span>
+                }
+              >
+                <Menu.ItemGroup>
+                  <Menu.Item key="4" style={{ fontSize: '16px' }}>Vous êtes un particulier</Menu.Item>
+                  <Menu.Item key="5" style={{ fontSize: '16px' }}>Vous êtes une collectivité</Menu.Item>
+                  <Menu.Item key="6" style={{ fontSize: '16px' }}>Vous êtes organisateur de foires et salons</Menu.Item>
+                  <Menu.Item key="7" style={{ fontSize: '16px' }}>Vous êtes un comité d'entreprise</Menu.Item>
+                  <Menu.Item key="8" style={{ fontSize: '16px' }}>Vous êtes une entreprise</Menu.Item>
+                  <Menu.Item key="9" style={{ fontSize: '16px' }}>Vous êtes un centre commercial</Menu.Item>
+                </Menu.ItemGroup>
+              </SubMenu>
+              <Menu.Item key="10">
+                <Link to="/about" />
+                Qui sommes nous ?</Menu.Item>
+            </Menu>
+          </Col>
+          <Col className="mobile">
+            <LittleMenu/>
+          </Col>
+        </Row>
       </Header>
       <Content style={{ marginTop: '80px' }}>
         <div style={{ background: '#fff', minHeight: 280 }}>{children}</div>
