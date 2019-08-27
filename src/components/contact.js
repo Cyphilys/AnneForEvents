39e448452919baa4beb6bdda63b6cbfb21e49757
Mickael kerkhove <mickael.kerkhove@coding-academy.fr>
2019-08-27 12:16:41 +0200

    Fix netlify form

--- src/components/contact.js
+++ src/components/contact.js
@@ -52,7 +52,7 @@ const CollectionCreateForm = Form.create({ name: 'form_in_modal' })(
           onCancel={onCancel}
           onOk={onCreate}
         >
-          <Form layout="vertical" data-netlify="true" netlify-honeypot="bot-field">
+          <Form name="contact" layout="vertical" data-netlify="true" netlify-honeypot="bot-field">
             <Form.Item>
               {getFieldDecorator('bot-field', {
                 rules: [{ required: false, message: "Merci de ne rien mettre ici"}],
